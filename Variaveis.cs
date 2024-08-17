41ba4d25c7cb169d528e50ca212f24df8d8f0ee1
Bruno Barbosa <brunobarbosa90@hotmail.com>
2024-08-16 21:06:43 -0300

    Add files via upload

---
+++ Variaveis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento = 1990;
+            int anoAtual = 2024;
+            int idade = 34;
+            string carro = "Corola";
+            string pais = "Brasil";
+            string estado = "Espirito Santo";
+            float velocidadeDoCarro = 60.0f;
+            float altura = 1.79f;
+            float habitantesPorArea = 234.456f;
+            bool sabeNadar = false;
+            bool sabeDirigir = true;
+            bool estrangeiro = false;
+
+            Console.WriteLine(anoNascimento);
+            Console.WriteLine(anoAtual);
+            Console.WriteLine(idade);
+            Console.WriteLine(carro);
+            Console.WriteLine(pais);
+            Console.WriteLine(estado);
+            Console.WriteLine(velocidadeDoCarro);
+            Console.WriteLine(altura);
+            Console.WriteLine(habitantesPorArea);
+            Console.WriteLine(sabeNadar);
+            Console.WriteLine(sabeDirigir);
+            Console.WriteLine(estrangeiro);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
